6afd56432f1fcb488ba391d8e8e335439322b36f
Shivamagrawal20 <shivamprfs@gmail.com>
2026-06-29 13:23:22 +0530

    [Shivam Agarwal].add Interfaces Scenerio based questions FitnessTracker,CouponValidator,SmartTV,ContentModeration

---
+++ oops-practice/gcr-codebase/Interfaces/Transport.java
@@ -0,0 +1,12 @@
+public interface Transport {
+    int a=10;
+    void add();
+
+    default void addTwo(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
