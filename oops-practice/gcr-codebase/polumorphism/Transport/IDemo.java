a5fa2315a021330341a0bbde61781022d601a360
Shivamagrawal20 <shivamprfs@gmail.com>
2026-06-29 08:58:29 +0530

    [Shivam Agrwal].add polymorphism assigmnet questions added

---
+++ oops-practice/gcr-codebase/polumorphism/Transport/IDemo.java
@@ -0,0 +1,15 @@
+package Transport;
+
+public interface IDemo {
+
+}
+//class DemoTwo{
+//    public static void main(String[] args) {
+//        IDemo d = new IDemo() {
+//        }
+//    }
+//}
+
+class DemoTwoImpl implements IDemo{
+    
+}
